6b5b63c8d3c4d3efeb2a06433c8b24654feef3a3
Lohitha <allamlohitha29@gmail.com>
2025-05-15 12:48:25 +0530

    Demo27 class committed

---
+++ src/test/java/sampleGit/Demo27.java
@@ -0,0 +1,12 @@
+package sampleGit;
+
+public class Demo27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+	 System.out.println("Hello world");
+		
+	}
+
+}
